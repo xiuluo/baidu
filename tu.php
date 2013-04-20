bdd3ef5a4a54d44ffdadc6f84ece577405b409d8
Shura <chen919695074@gmail.com>
2013-04-20 23:54:49 +0800

    修复图片外链功能

--- tu.php
+++ tu.php
@@ -2,7 +2,7 @@
 $canshu=$_SERVER["QUERY_STRING"];
 $wangzhi="http://pan.baidu.com/share/link?".$canshu;
 $file=file_get_contents($wangzhi);
-$pattern='/<a class="dbtn cancel b-fr" href=(.*?)id="downFileButtom">/is';
+$pattern='/<a class="new-dbtn" hidefocus="true" href=(.*?)id="downFileButtom">/is';
 preg_match_all($pattern,$file,$result);
 $tempurl=implode("",$result[1]);
 $fileurlt=str_replace("\"","",$tempurl);
