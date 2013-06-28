fafa4d35c2c6106fb6a5d8adbbed208ade1bd419
Shura <chen919695074@gmail.com>
2013-06-28 22:36:48 +0800

    修复下载地址失效的问题

--- down.php
+++ down.php
@@ -8,8 +8,8 @@ else
 {
 $wangzhi="http://pan.baidu.com/share/link?".$canshu;
 $file=file_get_contents($wangzhi);
-$pattern='/a><a class="new-dbtn" href=(.*?)id="downFileButtom">/i';
-preg_match_all($pattern,$file,$result);	
+$pattern='/a><a class="new-dbtn" hidefocus="true" href=(.*?)id="downFileButtom">/i';
+preg_match_all($pattern,$file,$result); 
 $tempurl=implode("",$result[1]);
 $fileurlt=str_replace("\"","",$tempurl);
 $fileurl=str_replace("&amp;","&",$fileurlt);
